b3feb39b9d32845ed8a389525f2e228607f171ad
VladMironchuk <vlad.mironchuk.02@gmail.com>
2019-12-26 19:30:48 +0300

    avatar

--- twitter/twitter-project/TWITTER-express/routes/twitter.js
+++ twitter/twitter-project/TWITTER-express/routes/twitter.js
@@ -3,49 +3,36 @@ const router = express.Router();
 const faker = require('faker/locale/ru');
 
 /* GET home page. */
-<<<<<<< HEAD
 router.get('/names', (req, res) => {
     var names = [
-=======
-router.get('/cities', (req, res) => {
-    var dates = [
->>>>>>> 9f812f4142d0828ce7eb243efedf7fe89abc27c0
     {
         "firstName": faker.name.firstName(),
-        "lastName": faker.name.lastName()
+        "lastName": faker.name.lastName(),
+        "img":faker.image.avatar()
     },
   {
         "firstName": faker.name.firstName(),
-        "lastName": faker.name.lastName()
-    },
-<<<<<<< HEAD
-      {
-        "firstName": faker.name.firstName(),
-        "lastName": faker.name.lastName()
+        "lastName": faker.name.lastName(),
+        "img":faker.image.avatar()
     },
       {
         "firstName": faker.name.firstName(),
-        "lastName": faker.name.lastName()
+        "lastName": faker.name.lastName(),
+        "img":faker.image.avatar()
     },
       {
         "firstName": faker.name.firstName(),
-        "lastName": faker.name.lastName()
+        "lastName": faker.name.lastName(),
+        "img":faker.image.avatar()
+    },
+      {
+        "firstName": faker.name.firstName(),
+        "lastName": faker.name.lastName(),
+        "img":faker.image.avatar()
     }
     ];
 
   res.render('names', {"names":names});
 });
-=======
-    {
-        "dates":"12.06.19"
-    }
-    ];
-
-  res.render('dates', {"dates":dates});
-});
-
-
->>>>>>> 9f812f4142d0828ce7eb243efedf7fe89abc27c0
-
 
 module.exports = router;
